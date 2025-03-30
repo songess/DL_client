176050ddeac6f30cb9c49b07cc67cb16664a7016
송은수 <songess@naver.com>
2025-03-30 21:31:41 +0900

    [feat] : 메타데이터 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,23 @@ const pretendard = localFont({
   weight: '45 920',
 });
 
+export const metadata: Metadata = {
+  title: 'Dreaming Library',
+  description: '서강대학교 도서 대여 시스템',
+  icons: {
+    icon: '/png/2025-03-30-21-20-53.png',
+  },
+  openGraph: {
+    title: 'Dreaming Library',
+    images: [
+      {
+        url: '/png/2025-03-30-21-20-53.png',
+      },
+    ],
+    type: 'website',
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
