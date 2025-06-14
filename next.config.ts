326123841a519f242c431063a5a40310bb84e8e3
songess <songess@naver.com>
2025-06-14 08:52:20 +0000

    [devops]: nextConfig env 추가

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,10 @@ const nextConfig: NextConfig = {
     host: '0.0.0.0',
     port: 3000,
   },
+  env: {
+    NEXT_PUBLIC_LOGROCKET_ID: process.env.NEXT_PUBLIC_LOGROCKET_ID,
+    NEXT_PUBLIC_VERCEL_ENV: process.env.NEXT_PUBLIC_VERCEL_ENV,
+  },
 };
 
 export default nextConfig;
